e79880da49bf15917e8b2ca134a971e639572d3c
marcos garcia-riego del fresno <156913875+Marcos12749@users.noreply.github.com>
2024-05-14 11:39:54 +0200

    Add files via upload

--- clientes.sql
+++ clientes.sql
@@ -67,3 +67,8 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_add_clientes`(IN `v_nombre` VARCHAR(50), IN `v_ciudad` VARCHAR(50), IN `v_facturacion` DECIMAL(8,2))
+INSERT INTO `clientes` (`id`, `nombre`, `ciudad`, `facturacion`) VALUES (NULL, v_nombre, v_ciudad, v_facturacion)$$
+DELIMITER ;
